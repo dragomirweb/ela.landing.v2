a7dc231e87d881cb33ace6e958bf47a46b56dc90
Daniel <dan@dragomirweb.com>
2022-01-24 00:34:40 +0200

    add gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 /* eslint-disable @next/next/no-page-custom-font */
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
@@ -24,8 +25,23 @@ export default class MyDocument extends Document {
             as="stylesheet"
           />
           <link rel="icon" href="/favicon.png" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-K6TPR3T');`,
+            }}
+          ></script>
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6TPR3T"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
